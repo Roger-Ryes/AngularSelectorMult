c54d691e03a423bc47a84e434dafcd91ef323a2e
Roger-Rys <roger.reyesm97@gmail.com>
2022-04-25 22:30:48 -0500

    estructura proyecto

--- src/app/country/pages/selector-page/selector-page.component.ts
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountryService } from '../../services/country-service.service';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
-  styleUrls: ['./selector-page.component.css']
+  styles: []
 })
 export class SelectorPageComponent implements OnInit {
 
-  constructor() { }
+  regions: string[] = [];
+
+  myForm: FormGroup = this.fb.group({
+    region: ['', [Validators.required]],
+
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private countServic: CountryService) { }
 
   ngOnInit(): void {
+    this.regions = this.countServic.regions;
+  }
+
+  save(){
+    console.log(this.myForm.value);
   }
 
 }
